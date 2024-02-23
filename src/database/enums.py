59e113a5832ca27918d5cae74c4293c6b80874ad
Roman505050 <myhun59@gmail.com>
2024-02-23 19:05:52 +0200

    upd model

--- src/database/enums.py
+++ src/database/enums.py
@@ -4,3 +4,16 @@ class Role(StrEnum):
     ADMIN = auto()
     MONITOR = auto()
     STUDENT = auto()
+
+class Week(StrEnum):
+    UPPER = auto()
+    LOWER = auto()
+
+class Day(StrEnum):
+    MONDAY = auto()
+    TUESDAY = auto()
+    WEDNESDAY = auto()
+    THURSDAY = auto()
+    FRIDAY = auto()
+    SATURDAY = auto()
+    SUNDAY = auto()
--- src/database/models.py
+++ src/database/models.py
@@ -16,7 +16,7 @@ from sqlalchemy.sql import func
 from typing import Annotated
 import datetime
 
-from src.database.enums import Role
+from src.database.enums import Role, Week, Day
 
 created_at = Annotated[datetime.datetime, mapped_column(DateTime(timezone=True), default=func.now())]
 updated_at = Annotated[datetime.datetime, mapped_column(DateTime(timezone=True), 
@@ -48,7 +48,40 @@ class Groups(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     course: Mapped[int] = mapped_column(Integer(1), nullable=False)
+    year: Mapped[int] = mapped_column(Integer(4), nullable=False)
+    profession: Mapped[int] = mapped_column(Integer(3), nullable=False)
+    monitor_id: Mapped[int] = mapped_column(Integer, ForeignKey("Students.id"), nullable=False)
+    admin_id: Mapped[int] = mapped_column(Integer, ForeignKey("Students.id"), nullable=False)
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
 
     students = relationship("Students", back_populates="group")
+    Schedule = relationship("Schedule", back_populates="group")
+
+class Schedule(Base):
+    __tablename__ = "Schedule"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    group_id: Mapped[int] = mapped_column(Integer, ForeignKey("Groups.id"), nullable=False)
+    week: Mapped[Week] = mapped_column(String(10), nullable=False)
+    day: Mapped[Day] = mapped_column(String(20), nullable=False)
+    couple: Mapped[int] = mapped_column(Integer(1), nullable=False)
+    lesson: Mapped[str] = mapped_column(String(255), nullable=False)
+    teacher: Mapped[str] = mapped_column(String(255), nullable=False)
+    classroom: Mapped[str] = mapped_column(String(10), nullable=False)
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+    group = relationship("Groups", back_populates="schedule")
+
+class Register(Base):
+    __tablename__ = "Register"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    student_id: Mapped[int] = mapped_column(Integer, ForeignKey("Students.id"), nullable=False)
+    presence: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    schedule_id: Mapped[int] = mapped_column(Integer, ForeignKey("Schedule.id"), nullable=False)
+    date: Mapped[datetime.date] = mapped_column(nullable=False)
+    active_to: Mapped[datetime.datetime] = mapped_column(nullable=False)
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
